5e57fcdafecb784c78aaedc515677ddaff62ef88
rhpetry <rafaelhpetry@gmail.com>
2020-07-11 19:09:04 -0300

    Parallax implementado

---
+++ gmtk_/Assets/scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public GameObject Camera;
+    public float parallaxEffectMultiplier = 1f;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.transform;
+        lastCameraPosition = cameraTransform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 deltaMoviment = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMoviment * parallaxEffectMultiplier;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
