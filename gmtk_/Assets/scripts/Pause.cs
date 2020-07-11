ffe866b8651412fbcf0d8d74ce539d329802e5b2
Gabriel <gabrielmuller2501@gmail.com>
2020-07-11 15:07:56 -0300

    Adicionadas funcionalidades de pause e restart. Necessario atribuir tela de pause ao Player

---
+++ gmtk_/Assets/scripts/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pausePane;
+    public bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        pausePane.SetActive(isPaused);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!pausePane.activeInHierarchy) {
+                PauseGame();
+            }
+            else {
+                ContinueGame();
+            }
+        }
+    }
+
+    private void PauseGame() {
+        Time.timeScale = 0;
+        isPaused = true;
+        pausePane.SetActive(isPaused);
+    }
+
+    private void ContinueGame() {
+        Time.timeScale = 1;
+        isPaused = false;
+        pausePane.SetActive(isPaused);
+    }
+}
