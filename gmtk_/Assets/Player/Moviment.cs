d33cbc9553ff991ab2d9f38e88839f887f9e4c71
rhpetry <rafaelhpetry@gmail.com>
2020-07-10 21:53:48 -0300

    Player adicionado

---
+++ gmtk_/Assets/Player/Moviment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+          moveSpeed = 10f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0f);
+
+        transform.Translate(moveSpeed * Time.deltaTime * direction.normalized);
+    }
+}
