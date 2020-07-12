3dd9748642d1fd4eca3211e3c909a56208588f0f
Gabriel <gabrielmuller2501@gmail.com>
2020-07-12 14:14:26 -0300

    Parte 2

--- gmtk_/Assets/scripts/Player.cs
+++ gmtk_/Assets/scripts/Player.cs
@@ -1,57 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     [SerializeField]
     private float _playerSpeed = 5f;
     [SerializeField]
     private float _gravity = 1.0f;
     [SerializeField]
-    private float _jumpHeight = 15.0f;
+    public float _jumpHeight = 20.0f;
     private float _yVelocity;
     private float _tempoEsperado = 3.0f;
-    private float _timer = 0; 
+    private float _timer = 0;
     private CharacterController _controller;
-
-    private GameObject[] _light;
-    private float timerdaluz = 0.3f;
-
-    public float _countSomething = 0;
 
     public float base_delay;
     public float limit_delay;
 
     // Start is called before the first frame update
-    void Start()
-    {
-
-        _light = GameObject.FindGameObjectsWithTag("light");
-
+    void Start() {
         _controller = GetComponent<CharacterController>();
         if (!_controller)
             Debug.LogError("Controller is null!");
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void Update() {
         MovimentaUsGuri();
-
-        if (timerdaluz < 0.2f)
-        {
-            _light[0].GetComponent<Light>().intensity = 16 - 80*timerdaluz;
-            timerdaluz += Time.deltaTime;
-        }
     }
 
-    void MovimentaUsGuri()
-    {
+    void MovimentaUsGuri() {
         // movimento horizontal :
         float HorizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(HorizontalInput, 0, 0);     
+        if (HorizontalInput < 0) {
+            gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+        }
+        else if (HorizontalInput > 0) {
+            gameObject.GetComponentInChildren<SpriteRenderer>().flipX = false;
+        }
+        Vector3 direction = new Vector3(HorizontalInput, 0, 0);
         Vector3 movement = direction * _playerSpeed;
 
         // verifica se tem chão :
@@ -61,9 +49,9 @@ public class Player : MonoBehaviour
         // contador :
         _timer += Time.deltaTime;
 
-        
-        if(_timer >= _tempoEsperado && _controller.isGrounded)
-        {
+
+        if (_timer >= _tempoEsperado && _controller.isGrounded) {
+            SoundManager.instance.JumpSound();
             _yVelocity = _jumpHeight;               // pula
             _tempoEsperado = Random.Range(base_delay, limit_delay);    // tempo para o próximo pulo
             _timer = 0;                             // reinicia o contador
@@ -71,35 +59,26 @@ public class Player : MonoBehaviour
 
 
         // alteração no movimento do jogador :
-        movement.y = _yVelocity;                    
-        _controller.Move(movement * Time.deltaTime);        
+        movement.y = _yVelocity;
+        _controller.Move(movement * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.tag == "Coletavel") {
-            _countSomething++;
-            _jumpHeight = _jumpHeight + 5f;
+        string descricao = "";
+        if (other.tag == "ColetavelBom") {
+            SoundManager.instance.EnergySound();
+            _jumpHeight += 5f;
+            descricao = "+5 Jump Power";
             Destroy(other.gameObject);
-
-            _light[0].GetComponent<Light>().intensity = 16f;
-            timerdaluz = 0;
         }
-
-        if (other.tag == "coletavel2")
-        {
-            _countSomething++;
-            _jumpHeight = 15f;
+        if (other.tag == "ColetavelRuim") {
+            SoundManager.instance.EnergySound();
+            _jumpHeight -= 5f;
+            descricao = "-5 Jump Power";
             Destroy(other.gameObject);
         }
 
-        if (other.tag == "ganhou_tag")
-        {
-            SceneManager.LoadScene("Credits");
-        }
-
+        GameObject.FindGameObjectWithTag("DescricaoColetavel").GetComponent<Text>().text = descricao;
+        GameObject.FindGameObjectWithTag("SpriteColetavel").GetComponent<Image>().sprite = other.GetComponent<SpriteRenderer>().sprite;
     }
-
-
-
-
 }
---
+++ gmtk_/Assets/scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField]
+    private AudioSource soundFX;
+
+    [SerializeField]
+    private AudioClip jumpClip;
+    [SerializeField]
+    private AudioClip energyClip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Update is called once per frame
+    public void JumpSound()
+    {
+        soundFX.clip = jumpClip;
+        soundFX.Play();
+    }
+    public void EnergySound()
+    {
+        soundFX.clip = energyClip;
+        soundFX.Play();
+    }
+
+}
